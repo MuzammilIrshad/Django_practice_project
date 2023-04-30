e5b78f26b50ce54f0c6c17caf93654a781af8565
Muzammil Irshad <muzammilirshad931@gmail.com>
2023-04-30 15:54:32 +0500

    passing dynamic values to the template html files

--- polls/views.py
+++ polls/views.py
@@ -3,5 +3,10 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return render(request, "index.html")
+    context = {
+       "name":"Muzammil irshad",
+       "age":25,
+       "isMale":True
+    }
+    return render(request, "index.html", context)
 
